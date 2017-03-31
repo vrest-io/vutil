923edeedfe847c2e9818c042ed48254bb52e38eb
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-03-31 21:24:34 +0530

    added body

---
+++ _methods/getBodyParams/index.js
@@ -0,0 +1,14 @@
+const jsonBody = require("body/json");
+
+function func(vars,methods,req,res){
+  var func = function(err,json){
+    if(typeof json === 'object' && json !== null){
+      vars.params.body = json;
+    }
+    req.emit('body-recieved');
+  };
+  jsonBody(req,res,func);
+  return vars.params;
+}
+
+module.exports = func;
