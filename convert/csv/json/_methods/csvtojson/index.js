db43f993ac951420fc6e517c2f261a29b588ed95
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-04-06 15:52:29 +0530

    sample file added

--- convert/csv/json/_methods/csvtojson/index.js
+++ convert/csv/json/_methods/csvtojson/index.js
@@ -2,7 +2,8 @@
 const csvtojson = require('csvjson'), path = require('path'), fs = require('fs');
 
 function func(vars,methods,req,res,next){
-  fs.readFile(vars.params.body.filePath,function(error,data){
+  fs.readFile(path.join(GLOBAL_APP_CONFIG.dataPath,vars.params.body.filePath),
+      function(error,data){
     if(error){
       next({ status : 400, message : error.message || 'FILE_NOT_FOUND' });
     } else {
