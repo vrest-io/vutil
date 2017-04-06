a433caf0e449ed7409f7b1b2f20dfbdc695a9671
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-04-06 16:52:13 +0530

    adding new li

--- convert/csv/json/_methods/csvtojson/index.js
+++ convert/csv/json/_methods/csvtojson/index.js
@@ -1,5 +1,6 @@
 
-const csvtojson = require('csvjson'), path = require('path'), fs = require('fs');
+const csvtojson = require('csvtojson'),
+      path = require('path'), fs = require('fs');
 
 function func(vars,methods,req,res,next){
   fs.readFile(path.join(GLOBAL_APP_CONFIG.dataPath,vars.params.body.filePath),
@@ -7,7 +8,14 @@ function func(vars,methods,req,res,next){
     if(error){
       next({ status : 400, message : error.message || 'FILE_NOT_FOUND' });
     } else {
-      next({ output : csvtojson.toObject(data.toString()) });
+      var out = [];
+      csvtojson(vars.params.body.options).fromString(data.toString())
+        .on('csv',(csvRow)=>{
+          out.push(csvRow);
+        })
+        .on('done',()=>{
+          next({ output : out });
+        });
     }
   });
 }
