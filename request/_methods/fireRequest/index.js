ed40f43682e854fdf794eae306dd2dea766f2262
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-04-07 16:32:28 +0530

    with encoders

--- request/_methods/fireRequest/index.js
+++ request/_methods/fireRequest/index.js
@@ -2,16 +2,41 @@
 const fs = require('fs'), path = require('path'),
       request = require('request').defaults({ json : true });
 
+const encoders = function(ab){
+  try {
+    return require(ab).encode();
+  } catch(erm) {
+    return false;
+  }
+};
+
+const getFile = function(ab){
+  if(typeof ab === 'string'){
+    return fs.createReadStream(ab);
+  } else if(typeof ab === 'object' && ab && typeof ab.filePath === 'string'){
+    var enc = ab.encode;
+    var ret = fs.createReadStream(ab.filePath);
+    if(typeof enc === 'string'){
+      var enc = encoders(enc);
+      if(enc){
+        ret.pipe(enc);
+      }
+    }
+    return ret;
+  } else {
+    return false;
+  }
+};
+
 function func(vars,methods,req,res,next){
-  var formData = {}, kl, kn, bd = vars.params.body.body;
+  var formData = {}, rs, kl, kn, bd = vars.params.body.body;
   if(typeof bd === 'object' && bd){
     for(var ky in bd){
       if(ky === 'attachments' && Array.isArray(bd[ky])){
         kn = bd[ky], kl = kn.length;
         for(var z=0;z<kl;z++){
-          if(typeof kn[z] === 'string'){
-            kn[z] = fs.createReadStream(kn[z]);
-          }
+          rs = getFile(kn[z]);
+          if(rs) { kn[z] = rs; }
         }
       } else {
         formData[ky] = bd;
@@ -24,8 +49,10 @@ function func(vars,methods,req,res,next){
         && GLOBAL_METHODS.isAlphaNum(vars.params.body.fileKey)){
       fl = vars.params.body.fileKey;
     }
-    formData[fl] =
-      fs.createReadStream(vars.params.body.filePath);
+    rs = getFile(vars.params.body);
+    if(rs) {
+      formData[fl] = rs;
+    }
   }
   var toSend = {
     method : vars.params.body.method,
@@ -44,8 +71,10 @@ function func(vars,methods,req,res,next){
     for(var z=0;z<ln;z++){
       if(bds[z].body && typeof bds[z].body === 'object'
           && GLOBAL_METHODS._isStr(bds[z].body.filePath)){
-            bds[z].body =
-              fs.createReadStream(bds[z].body.filePath);
+        rs = getFile(bds[z].body);
+        if(rs){
+          bds[z].body = getFile(bds[z].body);
+        }
       }
     }
     toSend.multipart = bds;
