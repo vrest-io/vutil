ebe8fda3de5cfcaccd75a621e39ac1052a45db50
Dheeraj Aggarwal <dheeraj.aggarwal@optimizory.com>
2017-04-14 13:59:56 +0530

    reader improvements

--- modules/reader/index.js
+++ modules/reader/index.js
@@ -1,4 +1,5 @@
-var path = require("path"), fs = require("fs");
+var path = require("path"), fs = require("fs"),
+mmm = require("mmmagic");
 
 function sendError(res,msg,st){
   res.writeHead(st, {"Content-Type": "application/json"});
@@ -13,34 +14,41 @@ function func(req, res) {
     return sendError(res,'`filename` is a required query parameter', 400);
   }
 
-  fs.stat(filename, function(err,exists) {
-    if(err) {
-      return sendError(res, err.message || err, 501);
-    }
-    if(!exists) {
-      return sendError(res, "File Not Found", 404);
-    }
-
-    if (!(exists.isFile())) {
-      return
-        sendError(res,"The path is a directory. Please provide a path of file", 406);
+  fs.stat(filename, function(err, stats) {
+    if(err) { 
+      if(err.code === "ENOENT"){
+        return sendError(res, "File Not Found", 404);
+      }
+      return sendError(res, err.message || err, 400);
     }
 
-    fs.readFile(filename, function(err, file) {
-      if(err) {
-        return sendError(res, err.message || err, 501);
+    if (!(stats.isFile())) {
+      return sendError(res,"The path is a directory. Please provide a path of file", 400);
+    }
+
+    var magic = new mmm.Magic(mmm.MAGIC_MIME_TYPE);
+    magic.detectFile(filename, function(err, contentType) {
+      if(err) return sendError(res, 'Error while detecting the mime type for file.', 400);
+      if(contentType.indexOf("text") === -1){
+        return sendError(res, 'File type not supported', 400);
       }
 
-      if(filename.endsWith('json')){
-        res.writeHead(200, { "Content-Type": "application/json" });
-      } else if(filename.endsWith('xml')){
-        res.writeHead(200, { "Content-Type": "application/xml" });
-      } else {
-        res.writeHead(200, { "Content-Type": "text/plain" });
-      }
+      fs.readFile(filename, function(err, file) {
+        if(err) {
+          return sendError(res, err.message || err, 501);
+        }
+        
+        if(filename.endsWith('json')){
+          res.writeHead(200, { "Content-Type": "application/json" });
+        } else if(filename.endsWith('xml')){
+          res.writeHead(200, { "Content-Type": "application/xml" });
+        } else {
+          res.writeHead(200, { "Content-Type": contentType });
+        }
 
-      res.write(file);
-      res.end();
+        res.write(file);
+        res.end();
+      });
     });
   });
 }
