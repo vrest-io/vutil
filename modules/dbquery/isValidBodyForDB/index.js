fa31202e062fc7ed5b67c4d5234da5b19f0fe9b0
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-04-15 12:59:05 +0530

    no query in mongodb

--- modules/dbquery/isValidBodyForDB/index.js
+++ modules/dbquery/isValidBodyForDB/index.js
@@ -11,7 +11,8 @@ function func(vars,methods){
     case 'oracle' :
       return (typeof query === 'string' && query.length);
     case 'mongodb' :
-      query = utils.lastValue(vars.params, 'body');
+      query = (utils.lastValue(vars.params, 'body', 'query') ||
+        utils.lastValue(vars.params, 'body'));
       vars.params.body.query = {};
       if(typeof query !== 'object' || query === null){
         query = { collection : typeof query === 'string' ? query : false };
