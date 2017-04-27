9db582b14d1160803ecd2e29c768fb2302212f1d
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-04-27 17:05:04 +0530

    xml changes

--- modules/request/parsers/xml.js
+++ modules/request/parsers/xml.js
@@ -1,8 +1,53 @@
 
-var xml2js = require('xml2js').parseString;//, builder = new xml2js.Builder({cdata : true});
+var DOMParser = new (require('xmldom').DOMParser)();
+
+// Changes XML to JSON, https://davidwalsh.name/convert-xml-json
+function xmlToJson(xml) {
+
+  // Create the return object
+  var obj = {};
+
+  if (xml.nodeType == 1) { // element
+    // do attributes
+    if (xml.attributes.length > 0) {
+    obj["@attributes"] = {};
+      for (var j = 0; j < xml.attributes.length; j++) {
+        var attribute = xml.attributes.item(j);
+        obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+      }
+    }
+  } else if (xml.nodeType == 3) { // text
+    obj = xml.nodeValue.trim();
+  } else if (xml.nodeType == 4) { // text
+    return xml.textContent;
+  }
+
+  // do children
+  if (xml.hasChildNodes()) {
+    for(var i = 0; i < xml.childNodes.length; i++) {
+      var item = xml.childNodes.item(i);
+      var nodeName = item.nodeName;
+      if (typeof(obj[nodeName]) == "undefined") {
+        obj[nodeName] = xmlToJson(item);
+      } else if(nodeName !== '#text'){
+        if (typeof(obj[nodeName].push) == "undefined") {
+          var old = obj[nodeName];
+          obj[nodeName] = [];
+          obj[nodeName].push(old);
+        }
+        obj[nodeName].push(xmlToJson(item));
+      }
+    }
+  }
+  return obj;
+};
+
+function parseString(xml){
+  return domParser.parseFromString(xml,'application/xml')
+}
 
 function afterString(data,next){
-  xml2js(data,next);
+  next(null, xmlToJson(parseString(data)));
 };
 
 module.exports = function(data,opts,next){
