0217ea0674ac7e827062ba690419e272905f7477
Ramesh Kumar <ramesh.kumar@optimizory.com>
2017-05-03 11:27:24 +0530

    for hex

--- modules/request/parsers/checksum.js
+++ modules/request/parsers/checksum.js
@@ -6,10 +6,22 @@ function getHash(type){
 }
 
 function calculateHash(stream,hash,next,into){
-  hash.update(stream);
-  next( null, hash.digest(into || 'hex'));
+  if(stream){
+    hash.update(stream);
+  }
+  if(typeof next === 'function'){
+    next( null, hash.digest(into || 'hex'));
+  }
 }
 
 module.exports = function(data,opts,next){
-  calculateHash(data,getHash(opts.hashType),next, opts.hexEncoding);
+  if(typeof data === 'string'){
+    calculateHash(data,,next, opts.hexEncoding);
+  } else {
+    var hash = getHash(opts.hashType);
+    data.on('readable', () => {
+      const dt = stream.read();
+      calculateHash(dt,hash,dt?false:next,opts.hexEncoding);
+    });
+  }
 }
