6f215b3b591e52fb57700f979088f1fa688335da
Dheeraj Aggarwal <dheeraj.aggarwal@optimizory.com>
2019-05-16 18:22:47 +0530

    merged from develop

--- modules/csv2json/index.js
+++ modules/csv2json/index.js
@@ -1,19 +1,28 @@
-
 const csvtojson = require('../../modules/request/parsers/csv2json'),
-      utils = require('../../utils'),
-      path = require('path'), fs = require('fs');
+  utils = require('../../utils'),
+  path = require('path'),
+  fs = require('fs');
 
-function func(req,res,next){
-  if(!req.body){
-    return res.send(400, { message : "Invalid request payload" });
+function func(req, res, next) {
+  if (!req.body) {
+    return res.send(400, {
+      message: "Invalid request payload"
+    });
   }
   var filePath = req.body.filePath || req.body.filepath;
-  if(!utils.isStr(filePath)){
-    return res.send(400, { message : "Parameter `filePath` was missing in request." });
+  if (!utils.isStr(filePath)) {
+    return res.send(400, {
+      message: "Parameter `filePath` was missing in request."
+    });
   }
-  csvtojson(fs.createReadStream(req.body.filePath),req.body.options,function(err,out){
-    if(err) res.send(400,{ message : err });
-    else res.send({ output : out });
+  csvtojson(fs.createReadStream(req.body.filePath), req.body.options, function (err, out) {
+    if (err) res.send(400, {
+      message: err
+    });
+    else res.send({
+      output: out,
+      filePath: req.body.filePath
+    });
   });
 }
 
--- modules/dbquery/index.js
+++ modules/dbquery/index.js
@@ -1,28 +1,52 @@
-
 const utils = require('../../utils'),
   appVars = require('../../vars.json'),
   isConnAvailable = require('./isConnAvailable'),
   isValidBodyForDB = require('./isValidBodyForDB'),
   mainVars = require('./vars.json'),
   methods = {
-    predb : require('./predb')
+    predb: require('./predb')
   };
 
-module.exports = function(req,res,next){
-  var nowVars = utils.assign({ params : { path : {}, body : {} } },mainVars);
-  utils.assign(nowVars.params.body,req.body);
-  utils.assign(nowVars.params.path,req.params);
-  if(isConnAvailable(nowVars)){
-    if(isValidBodyForDB(nowVars,methods)){
-      require('./'+nowVars.connType)(nowVars,methods,function(data){
-        res.status(data.status||200);
+module.exports = function (req, res, next) {
+  var nowVars = utils.assign({
+    params: {
+      path: {},
+      body: {}
+    }
+  }, mainVars);
+  utils.assign(nowVars.params.body, req.body);
+  utils.assign(nowVars.params.path, req.params);
+  if (isConnAvailable(nowVars)) {
+    if (isValidBodyForDB(nowVars, methods)) {
+      function after(data) {
+        res.status(data.status || 200);
         delete data.status;
         res.send(data);
-      });
+      }
+      switch (nowVars.connType) {
+        case 'mongodb':
+          return require('./mongodb')(nowVars, methods, after);
+        case 'mysql':
+          return require('./mysql')(nowVars, methods, after);
+        case 'mssql':
+          return require('./mssql')(nowVars, methods, after);
+        case 'oracle':
+          return require('./oracle')(nowVars, methods, after);
+        case 'postgres':
+          return require('./postgres')(nowVars, methods, after);
+        default:
+          res.send(400, {
+            "message": "Invalid connection type"
+          });
+      }
     } else {
-      res.send(400,{ "message":'Value of parameter `query` was not valid.' });
+      res.send(400, {
+        "message": 'Value of parameter `query` was not valid.'
+      });
     }
   } else {
-    res.send(400,{ "message":mainVars.messages[nowVars.conErrorType] });
+    res.send(400, {
+      "message": mainVars.messages[nowVars.conErrorType]
+    });
   }
 };
