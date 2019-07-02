0c98f9232cd3c8cbe4bf0a5ea7948c7b1a7d2061
Dheeraj Aggarwal <dheeraj.aggarwal@optimizory.com>
2019-07-02 15:21:21 +0530

    let converted to var for backward node compatibility

--- modules/request/parsers/xls2json.js
+++ modules/request/parsers/xls2json.js
@@ -4,9 +4,9 @@ var csv2json = require("./csv2json");
 module.exports = function (data, opts, next) {
   try {
     if (Buffer.isBuffer(data)) {
-      let wb = xlsx.read(data);
-      let ws = wb.Sheets[opts.sheet ? opts.sheet : wb.SheetNames[0]];
-      let csv = xlsx.utils.sheet_to_csv(ws, {
+      var wb = xlsx.read(data);
+      var ws = wb.Sheets[opts.sheet ? opts.sheet : wb.SheetNames[0]];
+      var csv = xlsx.utils.sheet_to_csv(ws, {
         blankrows: false
       });
       return csv2json(csv, opts.csvOpts || {
